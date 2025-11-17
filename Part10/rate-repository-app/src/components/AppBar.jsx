d83f0dba9e27ff45bd818b6359d59814c5fc6a54
Imamasharmeen <imamasharmeen@gmail.com>
2025-11-17 02:21:56 +0200

    Exercise 10.7: scrollable app bar

--- Part10/rate-repository-app/src/components/AppBar.jsx
+++ Part10/rate-repository-app/src/components/AppBar.jsx
@@ -1,9 +1,7 @@
 import { View, StyleSheet, ScrollView } from 'react-native'
 import Constants from 'expo-constants'
-import theme from '../theme'
+import theme from './theme'
 import AppBarTab from './AppBarTab'
-import { useQuery } from '@apollo/client'
-import { ME } from '../graphql/queries'
 
 const styles = StyleSheet.create({
   container: {
@@ -13,29 +11,11 @@ const styles = StyleSheet.create({
 })
 
 const AppBar = () => {
-  const { loading, error, data } = useQuery(ME)
-
-  if (loading) {
-    return null
-  }
-
   return (
     <View style={styles.container}>
-      <ScrollView horizontal>
+      <ScrollView style={styles.flex} horizontal>
         <AppBarTab text={'Repositories'} to='/' />
-
-        {data && data.me ? (
-          <>
-            <AppBarTab text={'Sign Out'} to='/signin' />
-            <AppBarTab text={'Create a review'} to='/createReview' />
-            <AppBarTab text={'My Reviews'} to='/myReviews' />
-          </>
-        ) : (
-          <>
-            <AppBarTab text={'Sign In'} to='/signin' />
-            <AppBarTab text={'Sign Up'} to='/signup' />
-          </>
-        )}
+        <AppBarTab text={'Sing In'} to='/signin' />
       </ScrollView>
     </View>
   )
--- Part10/rate-repository-app/src/components/AppBarTab.jsx
+++ Part10/rate-repository-app/src/components/AppBarTab.jsx
@@ -1,34 +1,21 @@
-import { Link, useNavigate } from 'react-router-native'
+import { Link } from 'react-router-native'
 import { StyleSheet, Pressable } from 'react-native'
 import Text from './Text'
-import useAuthStorage from '../hooks/useAuthStorage'
-import { useApolloClient } from '@apollo/client'
 
 const styles = StyleSheet.create({
   container: {
-    paddingBlock: 30,
-    paddingInline: 15,
+    padding: 20,
   },
 })
 
 const AppBarTab = ({ text, to }) => {
-  const navigate = useNavigate()
-  const authStorage = useAuthStorage()
-  const apolloClient = useApolloClient()
-
-  const onClick = () => {
-    if (text === 'Sign Out') {
-      authStorage.removeAccessToken()
-      apolloClient.resetStore()
-    }
-    navigate(to)
-  }
-
   return (
-    <Pressable onPress={onClick} style={styles.container}>
-      <Text color='white' fontWeight='bold' fontSize='subheading'>
-        {text}
-      </Text>
+    <Pressable style={styles.container}>
+      <Link to={to}>
+        <Text color='white' fontWeight='bold' fontSize='subheading'>
+          {text}
+        </Text>
+      </Link>
     </Pressable>
   )
 }
