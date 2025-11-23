d3bd184e4b419a2ad41d200f49f3ba7bf694a5a2
Imamasharmeen <imamasharmeen@gmail.com>
2025-11-23 02:49:32 +0200

    Exercises 13.5.-13.7.

--- Part13/blog-postgres/controllers/blogs.js
+++ Part13/blog-postgres/controllers/blogs.js
@@ -1,23 +1,6 @@
 const router = require('express').Router()
-const jwt = require('jsonwebtoken')
 
-const { Blog, User } = require('../models')
-const { SECRET } = require('../util/config')
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
-    } catch (error) {
-      return res.status(401).json({ error: 'token invalid' })
-    }
-  } else {
-    return res.status(401).json({ error: 'token missing' })
-  }
-
-  next()
-}
+const { Blog } = require('../models')
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
@@ -26,27 +9,16 @@ const blogFinder = async (req, res, next) => {
 
 router.get('/', async (req, res, next) => {
   try {
-    const blogs = await Blog.findAll({
-      attributes: { exclude: ['userId'] },
-      include: {
-        model: User,
-        attributes: ['name'],
-      },
-    })
+    const blogs = await Blog.findAll()
     res.json(blogs)
   } catch (error) {
     next(error)
   }
 })
 
-router.post('/', tokenExtractor, async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.decodedToken.id)
-    const blog = await Blog.create({
-      ...req.body,
-      userId: user.id,
-      date: new Date(),
-    })
+    const blog = await Blog.create(req.body)
     return res.json(blog)
   } catch (error) {
     next(error)
@@ -68,18 +40,14 @@ router.put('/:id', blogFinder, async (req, res, next) => {
   }
 })
 
-router.delete('/:id', tokenExtractor, blogFinder, async (req, res, next) => {
+router.delete('/:id', blogFinder, async (req, res, next) => {
   const blog = req.blog
   if (blog) {
-    if (blog.userId === req.decodedToken.id) {
-      try {
-        await blog.destroy()
-        res.status(204).end()
-      } catch (error) {
-        next(error)
-      }
-    } else {
-      return res.status(401).json({ error: 'unauthorized delete operation' })
+    try {
+      await blog.destroy()
+      res.status(204).end()
+    } catch (error) {
+      next(error)
     }
   } else {
     res.sendStatus(404)
